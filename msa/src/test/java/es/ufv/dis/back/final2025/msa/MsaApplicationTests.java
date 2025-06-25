20f97c0a28c39b0912956de1e50df74416d6af1d
miguel <miguel@example.com>
2025-06-25 13:12:54 +0200

    Creación del back

---
+++ msa/src/test/java/es/ufv/dis/back/final2025/msa/MsaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.final2025.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
